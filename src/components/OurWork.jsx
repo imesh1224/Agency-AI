25e112260609e7f35dcacf4d1c8ee28183a30039
imesh1224 <imeshnadun231@gmail.com>
2026-06-27 10:16:02 +0530

    create team section

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -26,7 +26,7 @@ const OurWork = () => {
   return (
     <div
       id="our-work"
-      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
       <Title
         title="Our latest work"
